07728f813f0f707c07c73fa5a3c97baec346a359
rripamonti <rripamonti@studenti.uninsubria.it>
2020-06-15 14:36:00 +0200

    Refactor

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,3 @@
-import anonymizer.Anonymizer;
-import extractor.UtilityExtractor;
-import org.hsqldb.jdbc.JDBCStatement;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class Main {
 
